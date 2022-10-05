969531604ca5c90f73ce93e7b46c3d9fac963c23
pyrolass <las.suleman1@gmail.com>
2022-10-05 16:23:40 +0300

    Initial Commit

---
+++ NYCSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchool
+//
+//  Created by Las Suleman on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
